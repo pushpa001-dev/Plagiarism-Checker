c042bf67e3401fd7eafeca82031a4ed63fcd283e
Pushpahas <pushpahas77@gmail.com>
2025-10-27 16:29:15 +0530

    Fix Procfile syntax for Render

--- plagiarism_checker/settings.py
+++ plagiarism_checker/settings.py
@@ -55,4 +55,4 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-ALLOWED_HOSTS = ['https://plagiarism-checker-b23o.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['https://plagiarism-checker-b23o.onrender.com', 'localhost', '127.0.0.1' , '192.168.1.12']
